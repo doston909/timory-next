67e6448fbb7583c6a088d8112540145a763ce0d3
Dostonbek Ahmadaliev <ahmadalievd384@gmail.com>
2025-11-20 22:56:29 +0900

    feat:DAVE/ meta tags integrations on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,30 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="titles" content="Timory" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        {/** SEO */}
+        <meta 
+        name="keyword" 
+        content={"timory, timory.uz, timory mern, mern nestjs fullstack"} 
+        />
+        <meta 
+        name="description" 
+        content={
+          "Buy watches anywhere anytime in our app. | " +
+          "Покупайте и продавайте недвижимость в любое время и в любом месте в Южной Корее. | " +
+          "언제 어디서나 대한민국에서 부동산을 사고파세요." 
+        }
+        />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
